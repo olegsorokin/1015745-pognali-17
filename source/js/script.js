17d1f51828e06d072ca07526aaf870b261c6df83
Ba2ev <Ba2evd@gmail.com>
2019-07-27 06:30:26 +0300

    index.html: частичная реализация header

--- source/js/script.js
+++ source/js/script.js
@@ -1,3 +1,17 @@
+/*---Открытие/закрытие меню в header---*/
+var menuButton = document.querySelector(".header__menu-button");
+
+menuButton.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  if (menuButton.classList.contains("menu-button--close") == true) {
+    menuButton.classList.remove("menu-button--close");
+  } else {
+    menuButton.classList.add("menu-button--close");
+  }
+});
+
+
+/*---Открытие/закрытие прайса с бизнес-тарифами---*/
 var priceLink = document.querySelector(".prices__link");
 var priceModal = document.querySelector(".modal-prices");
 var priceClose = document.querySelector(".modal-prices__close");
